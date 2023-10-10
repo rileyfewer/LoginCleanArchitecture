d84c5988c3659f53f78b6b1106739e4d9050e771
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-10-10 11:03:27 -0400

    pushing

--- src/view/SignupView.java
+++ src/view/SignupView.java
@@ -6,10 +6,7 @@ import interface_adapter.SignupViewModel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -54,7 +51,8 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         // in the usernameInputField. We're actually creating an anonymous class
         // here that is a subclass of KeyListener. Notice how it has access to
         // instance variables in the enclosing class.
-        usernameInputField.addKeyListener(new KeyListener() {
+        usernameInputField.addKeyListener(
+                new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
                 SignupState currentState = signupViewModel.getState();
@@ -68,6 +66,48 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
             @Override
             public void keyReleased(KeyEvent e) {}
         });
+
+        passwordInputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                SignupState currentstate = signupViewModel.getState();
+                char typedChar = e.getKeyChar();
+                currentstate.setPassword(currentstate.getPassword() + typedChar);
+                signupViewModel.setState(currentstate);
+                //super.keyTyped(e);
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+        });
+
+        passwordInputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //super.keyTyped(e);
+                SignupState currentstate = signupViewModel.getState();
+                char typedChar = e.getKeyChar();
+                currentstate.setRepeatPassword(currentstate.getPassword() + typedChar);
+                signupViewModel.setState(currentstate);
+
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+        });
+
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
@@ -77,7 +117,6 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         this.add(buttons);
 
     }
-
     /**
      * React to a button click that results in evt.
      */
